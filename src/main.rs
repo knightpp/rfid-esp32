b615b0d57e91776d8c163b5833ba0306e8d02e50
Danylo Kondratiev <knightpp@proton.me>
2023-09-25 18:51:11 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -35,18 +35,18 @@ fn main() -> anyhow::Result<()> {
 
     let peripherals = Peripherals::take().context("no peripherals")?;
 
+    let i2c = peripherals.i2c0;
+    let scl = peripherals.pins.gpio22;
+    let sda = peripherals.pins.gpio21;
+    let config = I2cConfig::new().baudrate(KiloHertz(100).into());
+    let i2c_driver = I2cDriver::new(i2c, sda, scl, &config)?;
     {
-        let i2c = peripherals.i2c0;
-        let scl = peripherals.pins.gpio22;
-        let sda = peripherals.pins.gpio21;
-        let config = I2cConfig::new().baudrate(KiloHertz(100).into());
-        let i2c = I2cDriver::new(i2c, sda, scl, &config)?;
-        let i2c = ssd1306::I2CDisplayInterface::new(i2c);
+        let i2c = ssd1306::I2CDisplayInterface::new(i2c_driver);
         let mut display = ssd1306::Ssd1306::new(i2c, DisplaySize128x64, DisplayRotation::Rotate180)
             .into_buffered_graphics_mode();
         display.init().map_err(|err| anyhow::anyhow!("{:?}", err))?;
         display
-            .set_brightness(Brightness::BRIGHT)
+            .set_brightness(Brightness::DIM)
             .map_err(|err| anyhow::anyhow!("{:?}", err))?;
         display.clear_buffer();
 
@@ -67,8 +67,11 @@ fn main() -> anyhow::Result<()> {
         .context("create spi driver")?;
     let config = spi::config::Config::new();
     let device = SpiDeviceDriver::new(&driver, Some(sda), &config)?;
-    let spi = SpiInterface::new(device);
-    let mut mfrc522 = mfrc522::Mfrc522::new(spi).init().context("init mfrc522")?;
+
+    let spi = SpiInterface::new(device).with_delay(|| {
+        delay::Delay::delay_us(1);
+    });
+    let mut mfrc522 = mfrc522::Mfrc522::new(spi).init()?;
 
     loop {
         let version = mfrc522.version()?;
